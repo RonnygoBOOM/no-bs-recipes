7f1a4f2eeb92c66dbe355336aff05945022b9727
Scheibelr <scheibelr@gmail.com>
2022-07-28 10:33:09 -0400

    environment setup and touchablehighlight experiment

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  Image,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
+  // debug using console.log, but remove for production to prevent them from slowing down your app
+  //console.log("app executed");
+  let x = 1;
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <TouchableHighlight onPress={() => console.log("tapped")}>
+        <Image
+          source={{
+            width: 200,
+            height: 200,
+            uri: "https://picsum.photos/200/200",
+          }}
+        />
+      </TouchableHighlight>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
